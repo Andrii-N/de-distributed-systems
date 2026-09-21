0c8b0bb37b777f89f26d70b24ed3f6f6bd959780
Andrii N. <naionnma7600@gmail.com>
2026-09-22 00:15:06 +0300

    secondary: start server

--- secondary/src/main/java/com/distsys/replicatedlog/secondary/SecondaryApplication.java
+++ secondary/src/main/java/com/distsys/replicatedlog/secondary/SecondaryApplication.java
@@ -7,17 +7,29 @@ import java.util.concurrent.Executors;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpServer;
 
+import com.distsys.replicatedlog.common.HttpSupport;
+
 
 public class SecondaryApplication {
     
     public SecondaryApplication() {}
 
-    public static void main (String args[]) throws IOException {
-        int port = Integer.parseInt(System.getenv().getOrDefault("PORT", "8081"));
+    public static void main (String[] args) throws IOException {
+        SecondaryApplication app = new SecondaryApplication();
+        int port = HttpSupport.readPort("8080");
         HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
-        server.createContext("/messages/");
+        server.createContext("/messages", app::handleGetMessage);
         server.setExecutor(Executors.newCachedThreadPool());
 
         server.start();
-    };
+    }
+
+    private void handleGetMessage(HttpExchange exchange) throws IOException {
+        if(!("GET".equalsIgnoreCase(exchange.getRequestMethod()))) {
+            HttpSupport.sendPlainText(exchange, 405, "Method not allowed");
+            return;
+        }
+        HttpSupport.sendJson(exchange, 200, "{\"status\":\"OK\"}");
+
+    }
 }
